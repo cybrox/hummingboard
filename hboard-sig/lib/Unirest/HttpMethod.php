a608612211792fb1c62c60623693ac86f0e006c1
Sven Marc Gehring <marc@cybrox.eu>
2014-02-21 10:55:12 +0100

    HBoard v2.0.1 - Reset and basic structure
    Completely resetted project and rebuilt basic structure. Might still
    change a lot since currently everything is just a test.

--- hboard-sig/lib/Unirest/HttpMethod.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Unirest;
-class HttpMethod
-{
-	const DELETE = "DELETE";
-	const GET = "GET";
-	const POST = "POST";
-	const PUT = "PUT";
-	const PATCH = "PATCH";
-}
--- src/js/core.js
+++ src/js/core.js
@@ -1,228 +1,150 @@
 /**
- * Hummingboard
- * Simple but elegant Hummingbird stats page
+ * Hummingboard - Hummingbird tools
  *
- * Written and copyrighted 2013+
- * by Sven Marc 'CybroX' Gehring
+ * Written and copyrighted 2014+
+ * by Sven Marc 'cybrox' Gehring
  *
- * Licensed under CC BY-SA 3.0
- * For additional informations, please read the
- * LICENSE.md file or the license deed at the
- * Creative Commons website.
+ * Licensed under CC BY-SA
  */
- 
- 
- 
-/**
- * Generate statistics
- *
- * This is the main function of the
- * generation process, it will call
- * all needed subfunctions.
- */
-function loadUserStats(username){
 
-	$('#landingPage').hide();
-	$('#loader').show();
-	
-	$.ajax({
-		type: 'GET',
-		dataType: 'json',
-		url: './src/api/index.php?user='+username,
-		success: function(jsonData){
-		
-			if(jsonData.state != 4){
-				embedLandingPage("User '"+username+"' not found, please try again with a different name.");
-				return false;
+var config = {
+	apiurl: ""
+}
+
+
+
+
+
+
+/**********************************************************
+ * Ember Init & Router
+ */
+HB = Ember.Application.create();
+
+HB.Router.map(function(){
+	this.resource("achievements");
+	this.resource("statistics");
+	this.resource("calendar");
+	this.resource("sigimg");
+});
+
+
+
+
+
+/**********************************************************
+ * Hummginboard Controllers
+ */
+HB.ApplicationController = Ember.Controller.extend({
+	pageName: function(){
+		var thisPath = this.get("currentPath");
+		var thisName = thisPath.replace("index", "");
+		var pageName = thisName.charAt(0).toUpperCase() + thisName.slice(1);
+
+		return (pageName == "") ? "" : " - " + pageName;
+	}.property("currentPath"),
+
+	userName: "",
+	userIsOn: false,
+	userIsOk: false,
+	pageSize: 0,
+
+	/**
+	 * Overwrite Controller init method
+	 * Add methods to read the current user and the current page height
+	 */
+	init: function(){
+		this.readUser();
+		this.readPage();
+		this._super();
+	},
+
+	/**
+	 * Request a user from Hummingbird
+	 * This will either load the name given by the PHP part (url parameter)
+	 * or a cookie on the user's computer to directly display content
+	 * for a specific user. If no username is given, the user will be
+	 * redirected to the index (/) page.
+	 */
+	readUser: function(){
+		var bodyName = $("body").attr("name");
+		var userName = "";
+		var userIsOn = true;
+
+		if(bodyName === "__undefined"){
+			if($.cookie('_hboard-user') === undefined){
+				userName = "";
+				userIsOn = false;
+
+				// this.transitionToRoute('index');
+			} else {
+				userName = $.cookie('_hboard-user');
 			}
-			
-			window.history.pushState({}, username+" ~ Hummingboard", "/"+username);
-			dataArray = jsonData.data;
-			
-			$('#loader').hide();
-			$('#statsPage').fadeIn('fast');
-			updateInterface(dataArray);
-			
-			generateStatsTypes(dataArray.animetypes);
-			generateStatsState(dataArray.animeallocation);
-			generateStatsRates(dataArray.animeratings);
-		},
-		error: function(xhr, status, error) {
-			console.log(xhr);
-			console.log(status);
-			console.log(error);
-			console.log("Something went wrong, please hit me up if this problem won't go away in the next few minutes.");
+		} else {
+			userName = bodyName
 		}
-	});
-}
 
+		this.set("userName", this.userName);
+		this.set("userIsOn", this.userIsOn);
+	},
 
-/**
- * Display landing page
- *
- * This function will display the landing
- * page if the user call the site without
- * any defined user name.
- */
-function embedLandingPage(inputValue){
-
-	if(inputValue !== "") $('#landingInput').attr("placeholder", inputValue);
-	
-	$('#loader').hide();
-	$('#statsPage').hide();
-	$('#landingPage').fadeIn('fast');
-}
-
-
-/**
- * Submit landing page
- */
-function submitLandingPage(){
-	loadUserStats($('#landingInput').val());
-}
-
-
-/**
- * Update interface
- */
-function updateInterface(dataArray){
-
-	var hummingLink = "http://hummingbird.me/users/"+dataArray.name+"/";
-
-	$('#useravatar').attr("src", dataArray.avatar);
-	$('#userimage').attr("href", hummingLink);
-	$('#userfont').attr("href", hummingLink);
-	$('#userfont').text(dataArray.hbname);
-	$('#anmc').text(dataArray.animeallocation.total.anime);
-	$('#epsc').text(dataArray.animeallocation.total.episodes);
-	$('#anitime').text(generateAnimeTime(dataArray.animetime));
-	
-	$('#headContainer').css("margin-top", "0px");
-	$('#graphState').css({"width": "80%","height": "80%"});
-	$('#graphRates').css({"width": "80%","height": "80%"});
-	$('#graphTypes').css({"width": "80%","height": "80%"});
-}
-
-
-/**
- * Generate anime time string
- *
- * This function will generate a string
- * that contains the user's watched anime
- * time.
- */
-function generateAnimeTime(totalTime){
-
-	years   = Math.floor(totalTime / 525948.766);
-	left    = totalTime % 525948.766;
-	months  = Math.floor(left / 43829.766);
-	left    = left % 43829.0639;
-	days    = Math.floor(left / 1440);
-	left    = left % 1440;
-	hours   = Math.floor(left / 60);
-	minutes = Math.floor(left % 60);
-	
-	return years+" Years, "+months+" Months, "+days+" Days, "+hours+" Hours, "+minutes+" Minutes";
-	
-}
-
-
-/**
- * Generate anime types stats
- *
- * Generate bars for the anime types
- * TV, Movie, Special, OVA, ONA
- */
-function generateStatsTypes(stats){
-	
-	/* Generate stats array */
-	var dataArray = [];
-	$.each(stats, function(key, value){
-		dataArray.push([value, key, 'orangeBar']);
-	});
-	
-	appendGraph("graphTypes", dataArray);
-}
-
-
-/**
- * Generate anime state stats
- *
- * Generate the stats for watching, planned
- * and so on, this involves two bars per
- * element, one for the anime and one for the
- * episodes.
- */
-function generateStatsState(stats){
-	var dataArray = [];
-	
-	delete stats.total;
-	
-	$.each(stats, function(key, value){
-		
-		/* Rename listpoints */
-		switch(key){
-			case "currently-watching": keyname = "watching"; break;
-			case "plan-to-watch":      keyname = "planned";  break;
-			case "on-hold":            keyname = "onhold";   break;
-			default:                   keyname = key;        break;
+	readPage: function(){
+		this.pageSize = window.innerHeight - 61 - 40;
+		window.onresize = function(){
+			this.pageSize = window.innerHeight - 61 - 40;
 		}
-	
-		dataArray.push([[value["anime"], value["episodes"]], keyname, ['orangeBar', 'blueBar']]);
-	});
-	
-	$('#graphState').html("");
-	$('#graphState').graph({
-		data: dataArray,
-		isdouble: true
-	});
-}
+	}
+});
 
 
-/**
- * Generate anime rating stats
- *
- * Generates the stats for the anime ratings
- * from 0.0 - 5.0 plus "-" (unrated)
+HB.IndexController = Ember.Controller.extend({
+	needs: ["application"],
+
+	userName: "",
+	userIsOn: false,
+	userIsOk: false,
+
+	actions: {
+		defineUser: function(){
+			if(this.userName == "") return false;
+
+			this.set("controllers.application.userName", this.userName);
+			this.set("controllers.application.userIsOn", true);
+
+			$.cookie('_hboard-user', this.userName);
+			// $.getJSON("", function(json){
+
+			// });
+			this.set("userIsOn", true)
+		}
+	}
+});
+
+
+HB.StatisticsController = Ember.Controller.extend({
+	needs: ["application"]
+});
+
+
+
+
+
+/**********************************************************
+ * Hummingboard Classes
  */
-function generateStatsRates(stats){
-	var dataArray = [];
-	
-	$.each(stats, function(key, value){
-		dataArray.push([[value["anime"], value["episodes"]], key, ['orangeBar', 'blueBar']]);
-	});
-	
-	$('#graphRates').html("");
-	$('#graphRates').graph({
-		data: dataArray,
-		isdouble: true
-	});
-}
+HB.User = Ember.Object.extend({
+	isValid: false,
+	avatar: "",
+	cover: "",
+	name: "",
 
+	requestUser: function(username){
+		return $.getJSON("").then(function(json){
 
-/**
- * Append graph to target
- */
-function appendGraph(target, dataArray){
-	$('#'+target).html("");
-	
-	var exampleContainer = $('#'+target);
-	graphSizeX = exampleContainer.width();
-	graphSizeY = exampleContainer.height();
-	
-	$('#'+target).graph({
-		data: dataArray,
-		isdouble: false
-	});
-}
+		});
+	}
+});
 
+HB.Library = Ember.Object.extend({
 
-/**
- * Resite the stats container
- *
- * This function will resize the stats container
- * every time the window is resized.
- */
-function resizeStatsContainer(){
-	$('statsContainer').css("height", ($('body').height() - 100)+"px");
-}
+});
